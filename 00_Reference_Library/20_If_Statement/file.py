3ab498e14b5ebe17069bed188c020eea9055b7d8
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-04-01 14:17:28 -1000

    Python

---
+++ 00_Reference_Library/20_If_Statement/file.py
@@ -0,0 +1,6 @@
+x = 10
+
+if x > 50:
+    print("x is larger than 50")
+else:
+    print("x is smaller than 50")
---
+++ 00_Reference_Library/30_Data_Types/file.py
@@ -0,0 +1,21 @@
+# PRIMATIVE DATA TYPE
+# Boolean value
+is_hungry = True
+
+# integers
+age = 35
+
+# String
+name = "Garett"
+
+# COMPOSITE TYPES
+# Tuples - cannot be modified after its created. can hold a group of values of mixed data types
+dog = ('bruce', 'lab', 12, False )
+print(dog[0])
+
+# Lists - similar to array
+empty_list = []
+friends = ['tj', 'kyle', 'paige']
+print(friends[1])
+friends.append('jeron') #similar to .push()
+print(friends)
