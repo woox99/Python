743c4ec13f33b7693e80912b1a74ee25d54e70a7
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-04-03 00:33:01 -1000

    Added to references

---
+++ 00_Reference_Library/70_Lists/file.py
@@ -0,0 +1,29 @@
+ninjas = ['Rozen', 'KB', 'Oliver']
+my_list = ['4', ['list', 'in', 'a', 'list'], 987]
+empty_list = []
+
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+
+salad = 3 * vegetables
+print(salad)
+
+# adding index to list
+nums = [1,2,3,4,5]
+nums.append(99)
+print(nums)
+
+nums_copy = nums
+print(nums_copy)
+
+# Slicing lists 
+words = ["start","going","till","the","end"]
+print(words[1:]) # prints ['going', 'till', 'the', 'end']
+print(words[:4]) # prints ['start', 'going', 'till', 'the']
+print(words[2:4]) # prints ['till', 'the']
+
+
+
