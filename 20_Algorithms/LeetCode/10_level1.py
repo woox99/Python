d0ae6e9735acb0b2f1bcf720a0caa86349718ea5
Garett.Janulewicz <janulewiczgarett@gmail.com>
2023-04-19 23:33:00 -1000

    added to algorithms

---
+++ 20_Algorithms/LeetCode/10_level1.py
@@ -0,0 +1,21 @@
+# # Given an integer x, return true if x is a palindrome, and false otherwise.
+# def isPalindrome(num):
+#     num = str(num)
+#     reverse_num = num[::-1]
+#     if reverse_num == num:
+#         print("true")
+#         return True
+#     else:
+#         print("false")
+#         return False
+
+# def sqrt(x):
+#     i = 0
+#     while i > -1:
+#         if x == 0:
+#             return 0
+#         elif i * i <= x and (i+1)*(i+1) > x:
+#             return i
+#         i += 1
+        
+# print(sqrt(9))
